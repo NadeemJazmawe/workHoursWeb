00e52b4c12573dcf0b5637f6524a386b358b1346
Jbareen Mohamad <jbareen.jeme@gmail.com>
2021-12-16 00:00:55 +0200

    add server side fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function handleServer() {
+    fetch('/test')
+      .then(r => r.json())
+      .then(data => { console.log(data); })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hi</h1>
+      <button type="submit" onClick={handleServer}>click</button>
     </div>
   );
 }
