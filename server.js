3b1d2682cc630849e613c446c4843e69c66907bd
Jbareen Mohamad <jbareen.jeme@gmail.com>
2021-12-17 16:29:58 +0200

    add schema

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ app.get('/test', (req, res)=>{
 })
 
 const url = "mongodb+srv://test-website567:Nn123456@nadeem-cluster.hevx3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ;//"mongodb://127.0.0.1:27017";
+mongoose.connect(url);
 
+const Users = mongoose.model('Users', { ID: String, Password:String, Email:String, Phone:String });
+
+const user = new Users({ ID: 'nadeem', Password:"admin", Email:"nadeem@gmail.com", Phone:"0500" });
+// user.save().then(() => console.log('meow'));
 
 app.post('/signup', (req, res)=>{
     const {ID, pass} = req.body;
