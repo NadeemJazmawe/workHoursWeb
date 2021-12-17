d5a7430570662d312dbb58631dc5fa9cae7a461d
Jbareen Mohamad <jbareen.jeme@gmail.com>
2021-12-17 18:00:45 +0200

    add switch router

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Login from './page/login/login.js';
 import Signup from './page/signup/signup';
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 function App() {
 
   function handleServer() {
@@ -11,10 +13,20 @@ function App() {
       .then(data => { console.log(data); })
   }
   return (
-    <div className="App">
-      {/* hello world */}
-      <Login />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Logoin</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login"><Login /></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
