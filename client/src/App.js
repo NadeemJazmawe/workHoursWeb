3e4c65b3aa8b803246e680df21622a3730e35118
Jbareen Mohamad <jbareen.jeme@gmail.com>
2021-12-17 18:07:45 +0200

    clear declared files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,4 @@
 // import logo from './logo.svg';
-import {
-  BrowserRouter as Router,
-  Route
-} from "react-router-dom";
 import './App.css';
 import Login from './page/login/login.js';
 import Signup from './page/signup/signup';
